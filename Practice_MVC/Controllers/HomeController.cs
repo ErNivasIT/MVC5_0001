c58c3815bdbf893ffdc56113d38d14348456bbaa
ErNivasIT <shri.mca2010@gmail.com>
2020-12-06 19:39:07 +0530

    OK Commit with Email on Invitation Facility

---
+++ Practice_MVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using GuestResponse.Model;
+using Practice_MVC.Model;
+
+namespace Practice_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hours = DateTime.Now.Hour;
+            ViewBag.Greeting = hours < 12 ? "Good Morning" : "Good Afrernoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse.Model.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                MailModel objModel = new MailModel();
+                objModel.From = "shri.mca2010@gmail.com";
+                objModel.To = guestResponse.Email;
+                objModel.Subject = "Regarding the Invitation of Party ";
+                string responseType = guestResponse.WillAttend.Value == true ? "Glad to hear that you are attending the party, See you there !!!"
+                    : "It would be have better that you attend, be first on the next party !!!";
+
+                objModel.Body = "Dear " + guestResponse.Name + ", <br/> Thanks for your valuable response regarding the Invitation !!!<br/>"+ responseType;
+
+                MailMessage mail = new MailMessage();
+                mail.To.Add(objModel.To);
+                mail.From = new MailAddress(objModel.From);
+                mail.Subject = objModel.Subject;
+                string Body = objModel.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("shri.mca2010@gmail.com", "GoodMail*123");// Enter senders User name and password
+                smtp.Send(mail);
+                //Email Logic
+                return View("Thanks", guestResponse);
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Thanks()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice_MVC/Model/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuestResponse.Model
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please Enter Your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please Enter Your Email")]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please Enter Your Phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
